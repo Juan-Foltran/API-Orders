ae2599bb24aa1f7b84bee71dc14935a67df6054d
Juan Teodoro Foltran <juan16foltran@gmail.com>
2026-05-14 14:25:21 -0300

    refactor: rename project files

---
+++ src/controllers/users/usersCreate.controller.ts
@@ -0,0 +1,30 @@
+import express, { type Request, type Response } from 'express';
+import { createUserSchema } from '../../schemas/createUser.schema.js';
+import { addUser } from '../../models/users/create.model.js';
+
+export const createUser = async (req: Request, res: Response) => {
+  const result = createUserSchema.safeParse(req.body);
+
+  if (!result.success) {
+    return res.status(400).json({
+      errors: result.error.flatten().fieldErrors,
+    });
+  }
+
+  const data = result.data;
+
+  try {
+    const createNewUser = await addUser(data);
+
+    return res.status(201).json(createNewUser);
+  } catch (err) {
+    if (err instanceof Error) {
+      return res.status(400).json({
+        message: err.message,
+      });
+    }
+    return res.status(500).json({
+      message: 'Erro desconhecido',
+    });
+  }
+};
---
+++ src/models/users/create.model.ts
@@ -0,0 +1,44 @@
+import bcrypt from 'bcrypt';
+import { type DataCreate } from './users.types.js';
+import { prisma } from '../../db/client.js';
+
+const userExists = async (userEmail: string) => {
+  const data = await prisma.users.findUnique({
+    where: {
+      email: userEmail,
+    },
+  });
+
+  return data;
+};
+
+const createUser = async (email: string, password: string, userAddress: string, userName: string) => {
+  const user = await prisma.users.create({
+    data: {
+      email,
+      password,
+      userAddress,
+      userName,
+    },
+  });
+  return user;
+};
+
+export const addUser = async (data: DataCreate) => {
+  const exists = await userExists(data.email);
+  if (exists) {
+    throw new Error('Usuário já existe');
+  }
+
+  try {
+    const hash = await bcrypt.hash(data.password, 10);
+    const newUser = await createUser(data.email, hash, data.userAddress, data.userName);
+    return {
+      username: newUser.userName,
+      message: 'Usuário criado com sucesso',
+    };
+  } catch (err) {
+    console.error(err);
+    throw new Error('Erro interno no servidor');
+  }
+};
