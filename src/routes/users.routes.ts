ba8b8ea234854f0cb22b9e0c897168dd94fe12df
Juan Teodoro Foltran <juan16foltran@gmail.com>
2026-05-20 15:11:08 -0300

    feat(login): create login route

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,6 +1,10 @@
 import { Router } from 'express';
 import { createUser } from '../controllers/users/usersCreate.controller.js';
+import { auth } from '../middlewares/auth.middleware.js';
+import { login } from '../controllers/users/login.controller.js';
+import { type Request, type Response } from 'express';
 
 export const route = Router();
 
 route.post('/createUser', createUser);
+route.post('/login', login);
