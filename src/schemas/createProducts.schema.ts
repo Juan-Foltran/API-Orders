00664b5afcc5e0a3b39a0428773991dbe0337d73
Juan Teodoro Foltran <juan16foltran@gmail.com>
2026-07-02 14:19:41 -0300

    feat(products): add schema validation with zod

---
+++ src/schemas/createProducts.schema.ts
@@ -0,0 +1,10 @@
+import z from 'zod';
+
+export const createProductSchema = z.object({
+  title: z.string().min(1, 'Preencha esse campo').max(70, 'Titulo muito grande'),
+  description: z.string().min(1, 'preencha esse campo').max(300, 'Descrição muito grande'),
+  price: z
+    .number()
+    .positive()
+    .refine((value) => Number.isInteger(value * 100), 'O preço deve ter no m[aximo duas casas decimais'),
+});
