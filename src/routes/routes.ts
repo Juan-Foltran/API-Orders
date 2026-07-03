2c0826d608caf65a33254ecc94d6b3e9be4d1c6f
Juan Teodoro Foltran <juan16foltran@gmail.com>
2026-07-03 17:13:01 -0300

    feat(products): adding route to create products

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -5,6 +5,8 @@ import { login } from '../controllers/users/login.controller.js';
 import { newRequest } from '../controllers/stores/RequestNewStore.controller.js';
 import { listing } from '../controllers/stores/listRequest.controller.js';
 import { authAdm } from '../middlewares/authAdm.middleware.js';
+import { authOwner } from '../middlewares/authOwner.middleware.js';
+import { creation } from '../controllers/stores/createProducts.controller.js';
 
 export const route = Router();
 
@@ -17,3 +19,4 @@ route.post('/requestStore', authMiddleware, newRequest);
 
 // Admin routes
 route.get('/requests', authAdm, listing);
+route.post('/post-new-product', authOwner, creation);
