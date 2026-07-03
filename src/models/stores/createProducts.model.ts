813b893ed231cad9560cb73b3029ef874791287b
Juan Teodoro Foltran <juan16foltran@gmail.com>
2026-07-03 14:14:52 -0300

    feat(products): adding model that fetches id of stores linked to the user

--- src/models/stores/createProducts.model.ts
+++ src/models/stores/createProducts.model.ts
@@ -1,18 +1,35 @@
 import { prisma } from '../../db/client.js';
 import { type createProduct } from './stores.types.js';
 
-export const createProducts = async (product: createProduct, idStore: number) => {
+export const getStores = async (id: number) => {
+  const result = await prisma.users.findUnique({
+    where: {
+      id,
+    },
+    select: {
+      stores: {
+        select: {
+          idStore: true,
+        },
+      },
+    },
+  });
+
+  return result?.stores ?? [];
+};
+
+export const createProducts = async (product: createProduct) => {
   try {
     const creationProduct = await prisma.products.create({
       data: {
         name: product.title,
         description: product.description,
         price: product.price,
-        idStore: idStore,
+        idStore: product.idStore,
       },
     });
 
-    return creationProduct.name;
+    return creationProduct;
   } catch (err) {
     throw new Error('Erro ao criar o produto');
   }
